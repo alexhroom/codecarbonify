f81e837aa11de9c5b143b226cec2c58fa9e6f44b
alexhroom <alex.room@btinternet.com>
2022-11-03 10:55:12 +0000

    Initial script

---
+++ codecarbonify.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+
+from codecarbon import EmissionsTracker
+
+def main():
+    """Wraps codecarbon tracking around any given shell command."""
+
+    parser = argparse.ArgumentParser(description='Wraps codecarbon tracking '
+                                                 'around any given shell command.')
+
+    # positional arguments
+    parser.add_argument(
+        'command',
+        type=str,
+        help='The shell command to be executed.'
+        )
+
+    parser.add_argument(
+        '--name', '-n',
+        type=str,
+        default=None,
+        help='The name for the entry in the codecarbon spreadsheet. '
+             'If no name is given, then defaults to the command provided.'
+        )
+
+    args = parser.parse_args()
+    cmd = args.command
+    name = args.name or cmd  # default to name of command if no name given
+
+    tracker = EmissionsTracker(project_name=name)
+    tracker.start()
+    os.system(cmd)
+    tracker.stop()
